51522400ea2d12715f4c69a17b9be0c8523baacd
naboroday <86660091+naboroday@users.noreply.github.com>
2023-06-16 11:59:15 +0300

    Update dds_repository.py

--- service_dds/src/dds_loader/repository/dds_repository.py
+++ service_dds/src/dds_loader/repository/dds_repository.py
@@ -4,6 +4,8 @@ from typing import Any, Dict, List
 
 from lib.pg import PgConnect
 from pydantic import BaseModel
+import hashlib
+from decimal import Decimal
 
 class DdsRepository:
     def __init__(self, db: PgConnect) -> None:
@@ -24,7 +26,10 @@ class DdsRepository:
             'load_src': load_src
         }
 
-        self._execute_query(query, params)
+        result=self._execute_query(query, params)
+        inserted_h_user = result.fetchone()[0]  # Получить вставленное значение h_user_pk
+
+        return inserted_h_user
 
     def insert_h_product(self, product_id: str, load_src: str) -> None:
         query = """
@@ -41,7 +46,10 @@ class DdsRepository:
             'load_src': load_src
         }
 
-        self._execute_query(query, params)
+        result=self._execute_query(query, params)
+        inserted_h_product_pk = result.fetchone()[0]  # Получить вставленное значение h_product_pk
+
+        return inserted_h_product_pk
 
     def insert_h_category(self, category_name: str, load_src: str) -> None:
         query = """
@@ -58,7 +66,10 @@ class DdsRepository:
             'load_src': load_src
         }
 
-        self._execute_query(query, params)
+        result=self._execute_query(query, params)
+        inserted_h_category_pk = result.fetchone()[0]  # Получить вставленное значение h_category_pk
+
+        return inserted_h_category_pk
 
     def insert_h_restaurant(self, restaurant_id: str, load_src: str) -> None:
         query = """
@@ -75,7 +86,10 @@ class DdsRepository:
             'load_src': load_src
         }
 
-        self._execute_query(query, params)
+        result=self._execute_query(query, params)
+        inserted_h_restaurant_pk = result.fetchone()[0]  # Получить вставленное значение h_restaurant_pk
+
+        return inserted_h_restaurant_pk
 
     def insert_h_order(self, order_id: int, order_dt: datetime, load_src: str) -> None:
         query = """
@@ -93,7 +107,195 @@ class DdsRepository:
             'load_src': load_src
         }
 
+        result=self._execute_query(query, params)
+        inserted_h_order_pk = result.fetchone()[0]  # Получить вставленное значение h_order_pk
+
+        return inserted_h_order_pk
+
+    
+    def insert_l_order_product(self, h_order_pk: uuid.UUID, h_product_pk: uuid.UUID, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.l_order_product (h_order_pk, h_product_pk, load_src)
+            VALUES (%(h_order_pk)s, %(h_product_pk)s, %(load_src)s)
+            ON CONFLICT DO NOTHING
+        """
+        hk_order_product_pk = uuid.uuid4()
+
+        params = {
+            'hk_order_product_pk': hk_order_product_pk,
+            'h_order_pk': h_order_pk,
+            'h_product_pk': h_product_pk,
+            'load_src': load_src
+        }
+
         self._execute_query(query, params)
+
+
+    def insert_l_product_restaurant(self, h_restaurant_pk: uuid.UUID, h_product_pk: uuid.UUID, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.l_product_restaurant (hk_product_restaurant_pk, h_restaurant_pk, h_product_pk, load_src)
+            VALUES (%(hk_product_restaurant_pk)s, %(h_restaurant_pk)s, %(h_product_pk)s, %(load_src)s)
+            ON CONFLICT DO NOTHING
+        """
+        hk_product_restaurant_pk = uuid.uuid4()
+
+        params = {
+            'hk_product_restaurant_pk': hk_product_restaurant_pk,
+            'h_restaurant_pk': h_restaurant_pk,
+            'h_product_pk': h_product_pk,
+            'load_src': load_src
+        }
+
+        self._execute_query(query, params)        
+
+    def insert_l_product_category(self, h_category_pk: uuid.UUID, h_product_pk: uuid.UUID, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.l_product_category (hk_product_category_pk, h_category_pk, h_product_pk, load_src)
+            VALUES (%(hk_product_category_pk)s, %(h_category_pk)s, %(h_product_pk)s, %(load_src)s)
+            ON CONFLICT DO NOTHING
+        """
+        hk_product_category_pk = uuid.uuid4()
+
+        params = {
+            'hk_product_category_pk': hk_product_category_pk,
+            'h_category_pk': h_category_pk,
+            'h_product_pk': h_product_pk,
+            'load_src': load_src
+        }
+
+        self._execute_query(query, params)
+
+
+    def insert_l_order_user(self, h_order_pk: uuid.UUID, h_user_pk: uuid.UUID, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.l_order_user (hk_order_user_pk, h_order_pk, h_user_pk, load_src)
+            VALUES (%(hk_order_user_pk)s, %(h_order_pk)s, %(h_user_pk)s, %(load_src)s)
+            ON CONFLICT DO NOTHING
+        """
+        hk_order_user_pk = uuid.uuid4()
+
+        params = {
+            'hk_order_user_pk': hk_order_user_pk,
+            'h_order_pk': h_order_pk,
+            'h_user_pk': h_user_pk,
+            'load_src': load_src
+        }
+
+        self._execute_query(query, params)
+
+ 
+    def insert_s_user_names(self, h_user_pk: uuid.UUID, username: str, userlogin: str, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.s_user_names (h_user_pk, username, userlogin, load_dt, load_src, hk_user_names_hashdiff)
+            VALUES (%(h_user_pk)s, %(username)s, %(userlogin)s, %(load_dt)s, %(load_src)s, %(hk_user_names_hashdiff)s)
+            ON CONFLICT DO NOTHING
+        """
+        load_dt = datetime.now()
+
+        # Преобразование строки username в UUID с использованием хеша
+        name_hash = hashlib.sha1(username.encode('utf-8')).hexdigest()
+        hk_user_names_hashdiff = uuid.UUID(name_hash)
+
+        params = {
+            'h_user_pk': h_user_pk,
+            'username': username,
+            'userlogin': userlogin,
+            'load_dt': load_dt,
+            'load_src': load_src,
+            'hk_user_names_hashdiff': hk_user_names_hashdiff
+        }
+
+        self._execute_query(query, params)   
+
+
+    def insert_s_product_names(self, h_product_pk: uuid.UUID, name: str, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.s_product_names (h_product_pk, name, load_dt, load_src, hk_product_names_hashdiff)
+            VALUES (%(h_product_pk)s, %(name)s, %(load_dt)s, %(load_src)s, %(hk_product_names_hashdiff)s)
+            ON CONFLICT DO NOTHING
+        """
+        load_dt = datetime.now()
+
+        # Преобразование строки username в UUID с использованием хеша
+        name_hash = hashlib.sha1(name.encode('utf-8')).hexdigest()
+        hk_product_names_hashdiff = uuid.UUID(name_hash)
+
+        params = {
+            'h_product_pk': h_product_pk,
+            'name': name,
+            'load_dt': load_dt,
+            'load_src': load_src,
+            'hk_product_names_hashdiff': hk_product_names_hashdiff
+        }
+
+        self._execute_query(query, params)          
+
+    def insert_s_restaurant_names(self, h_restaurant_pk: uuid.UUID, name: str, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.s_restaurant_names (h_restaurant_pk, name, load_dt, load_src)
+            VALUES (%(h_restaurant_pk)s, %(name)s, %(load_dt)s, %(load_src)s, %(hk_restaurant_names_hashdiff)s)
+            ON CONFLICT DO NOTHING
+        """
+        load_dt = datetime.now()
+
+        # Преобразование строки username в UUID с использованием хеша
+        name_hash = hashlib.sha1(name.encode('utf-8')).hexdigest()
+        hk_restaurant_names_hashdiff = uuid.UUID(name_hash)
+
+        params = {
+            'h_restaurant_pk': h_restaurant_pk,
+            'name': name,
+            'load_dt': load_dt,
+            'load_src': load_src,
+            'hk_restaurant_names_hashdiff': hk_restaurant_names_hashdiff
+        }
+
+        self._execute_query(query, params)
+
+    def insert_s_order_cost(self, h_order_pk: uuid.UUID, cost: Decimal, payment: Decimal, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.s_order_cost (h_order_pk, cost, payment, load_dt, load_src, hk_order_cost_hashdiff)
+            VALUES (%(h_order_pk)s, %(cost)s, %(payment)s, %(load_dt)s, %(load_src)s, %(hk_order_cost_hashdiff)s)
+            ON CONFLICT DO NOTHING
+        """
+        load_dt = datetime.now()
+
+        # Преобразование строки username в UUID с использованием хеша
+        name_hash = hashlib.sha1(cost.encode('utf-8')).hexdigest()
+        hk_order_cost_hashdiff = uuid.UUID(name_hash)
+
+        params = {
+            'h_order_pk': h_order_pk,
+            'cost': cost,
+            'payment': payment,
+            'load_dt': load_dt,
+            'load_src': load_src,
+            'hk_order_cost_hashdiff': hk_order_cost_hashdiff
+        }
+
+        self._execute_query(query, params)       
+
+    def insert_s_order_status(self, h_order_pk: uuid.UUID, status: str, load_src: str) -> None:
+        query = """
+            INSERT INTO dds.s_order_status (h_order_pk, status, load_dt, load_src, hk_order_status_hashdiff)
+            VALUES (%(h_order_pk)s, %(status)s, %(load_dt)s, %(load_src)s, %(hk_order_status_hashdiff)s)
+            ON CONFLICT DO NOTHING
+        """
+        load_dt = datetime.now()
+
+        # Преобразование строки username в UUID с использованием хеша
+        name_hash = hashlib.sha1(status.encode('utf-8')).hexdigest()
+        hk_order_status_hashdiff = uuid.UUID(name_hash)
+
+        params = {
+            'h_order_pk': h_order_pk,
+            'status': status,
+            'load_dt': load_dt,
+            'load_src': load_src,
+            'hk_order_status_hashdiff': hk_order_status_hashdiff
+        }
+
+        self._execute_query(query, params)        
 
     def _execute_query(self, query: str, params: Dict[str, Any]) -> None:
         self._db.execute(query, params)
